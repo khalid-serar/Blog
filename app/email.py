10bcbb9a6e26f175e805cecb57d08922fae5750c
khalid-serar <khalid.serar@student.moringaschool.com>
2021-11-13 10:13:17 +0300

    commmit all changes

--- app/email.py
+++ app/email.py
@@ -1,11 +1,9 @@
-
-  
 from flask_mail import Message
 from flask import render_template
 from . import mail
 
 def mail_message(subject,template,to,**kwargs):
-    sender_email = 'ashrafzani585@gmail.com'
+    sender_email = 'khalid.serar@student.moringaschool.com'
 
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
